d86e190e25fb8c5c09409751cc9491aa8a408920
nithzhizn-cpu <nithzhizn@gmail.com>
2025-11-17 15:46:53 +0200

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,28 +1,26 @@
 from pydantic import BaseModel
 from typing import Optional, List
-from datetime import datetime
-
 
 class RegisterRequest(BaseModel):
     username: str
-    telegram_id: Optional[int] = None
-
+    telegram_id: Optional[str] = None
 
 class UserOut(BaseModel):
     id: int
     username: str
 
-    class Config:
-        from_attributes = True
+class PubKeyUpdate(BaseModel):
+    pubkey: str
 
+class PubKeyOut(BaseModel):
+    pubkey: str
 
 class MessageCreate(BaseModel):
     to: int
     iv: str
     ciphertext: str
-    msg_type: str = "text"
+    msg_type: Optional[str] = "text"
     ttl_sec: Optional[int] = None
-
 
 class MessageOut(BaseModel):
     id: int
@@ -30,22 +28,6 @@ class MessageOut(BaseModel):
     to_id: int
     iv: str
     ciphertext: str
-    msg_type: str
-    ttl_sec: Optional[int]
-    created_at: datetime
-
-    class Config:
-        from_attributes = True
-
 
 class MessagesResponse(BaseModel):
     messages: List[MessageOut]
-
-
-# E2EE pubkey
-class PubKeyUpdate(BaseModel):
-    pubkey: str   # JWK JSON як строка
-
-
-class PubKeyOut(BaseModel):
-    pubkey: str
